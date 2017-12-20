b6f2444718fb20d5ffd4d8fe3157a2c2e1082179
ACDM\772515 <sifukada@gmail.com>
2017-12-20 16:10:32 -0700

    Password hashed with md5

--- SignUp-LogIn/SignUp-LogIn/Form1.cs
+++ SignUp-LogIn/SignUp-LogIn/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,12 +18,24 @@ namespace SignUp_LogIn
             InitializeComponent();
         }
         UserDB newUsers = new UserDB(); //Instantiate UserDB list object
+
+        //Encrypt password with MD5
+        static string Encrypt (string Value)
+        {
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                UTF8Encoding utf8 = new UTF8Encoding();
+                byte[] data = md5.ComputeHash(utf8.GetBytes(Value));
+
+                return Convert.ToBase64String(data);
+            }
+        }
         
         //Sign up event
         private void btnSignUp_Click(object sender, EventArgs e)
         { 
             string username = txtUserName.Text;
-            string password = txtPassword.Text;
+            string password = Encrypt(txtPassword.Text);// Encrypting password with md5 here
 
             User newUser = new User(username,password);//Instantiate User object
             newUsers.AddToList(newUser); //Add newUser object to UserDB list
