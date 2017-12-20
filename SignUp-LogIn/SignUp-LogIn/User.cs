8203b9f289489dbba8f08604166457c4203bbd6a
ACDM\772515 <sifukada@gmail.com>
2017-12-20 11:53:54 -0700

    Added User and UserDB classes

---
+++ SignUp-LogIn/SignUp-LogIn/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignUp_LogIn
+{
+    //Class to store user objects
+    class User
+    {
+    }
+}
---
+++ SignUp-LogIn/SignUp-LogIn/UserDB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignUp_LogIn
+{
+    //Class with methods to read and write list to file
+    class UserDB
+    {
+    }
+}
